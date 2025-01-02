4e7ad67d8f61ca1afa441b48d93ce02bc9855db2
Femaro Dotecci <94189167+femaro1994@users.noreply.github.com>
2025-01-02 14:49:37 +0100

    Fix incorrect file extensions in uploaded documents

--- Prova 04.py
+++ Prova 04.py
@@ -18,5 +18,5 @@ while True:
     else:
         print("O número é menor. Tente novamente.")
 
-print("O número adivinhado foi:", numero_secreto)
+print("O número adivinhado foi:", numero_secreto) VzSM8CTqX6
 print("Número de tentativas:", tentativas)
