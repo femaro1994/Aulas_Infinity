c609b5dd826da00d9ef4c267b55504729ccd3e25
Femaro Dotecci <94189167+femaro1994@users.noreply.github.com>
2025-01-02 18:55:15 +0100

    Fix Inconsistent Date Formats Across Modules

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
-num = int(input("Digite o número da tabuada: "))
+num = int(input("Digite o número da tabuada: ")) H2RqwpxPQy
  qPDm270pXh
 for i in range(1, 11):
     print(f"{num} x {i} = {num * i}")
