6af31178fd0d623ec3c51bfd8d023a5cad2d25c4
Femaro Dotecci <94189167+femaro1994@users.noreply.github.com>
2025-01-02 18:55:22 +0100

    Add Validation for Signup Form

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
-num = int(input("Digite o número da tabuada: ")) H2RqwpxPQy
+num = int(input("Digite o número da tabuada: "))
  qPDm270pXh
 for i in range(1, 11):
     print(f"{num} x {i} = {num * i}")
